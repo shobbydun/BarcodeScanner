84387665c3eb8c18ded4eb4d956e134eb2f4d533
KItavi Duncan Gitau <87327873+shobbydun@users.noreply.github.com>
2024-05-05 16:42:23 +0300

    Update ExampleInstrumentedTest.kt

--- app/src/androidTest/java/com/febrian/qrbarcodescanner/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/febrian/qrbarcodescanner/ExampleInstrumentedTest.kt
@@ -11,7 +11,7 @@ import org.junit.Assert.*
 /**
  * Instrumented test, which will execute on an Android device.
  *
- +254 715 742166
+ +254 715 742166 - njambazi
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 @RunWith(AndroidJUnit4::class)
